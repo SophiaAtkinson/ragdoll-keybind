3eb8068628206633d394cb97f89827382bace842
ethancantcode <61561333+ethancantcode@users.noreply.github.com>
2020-04-16 18:43:02 -0700

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,8 +1,7 @@
 Citizen.CreateThread(function() 	
     while true do 		
         Citizen.Wait(0) 		
-        if IsControlPressed(1, 73 --[[ "x" key ]]) then 		
-            -- https://runtime.fivem.net/doc/natives/#_0xAE99FB955581844A
+        if IsControlPressed(1, 303 --[[ "u" key ]]) then 		
             SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false) 
         end 	
     end 

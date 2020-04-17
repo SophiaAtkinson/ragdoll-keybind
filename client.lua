d840157cde73480e0684c139ccb97e1f5846b353
ethancantcode <61561333+ethancantcode@users.noreply.github.com>
2020-04-16 18:44:14 -0700

    Changed The Key From X To U

--- client.lua
+++ client.lua
@@ -2,6 +2,7 @@ Citizen.CreateThread(function()
     while true do 		
         Citizen.Wait(0) 		
         if IsControlPressed(1, 303 --[[ "u" key ]]) then 		
+            -- https://runtime.fivem.net/doc/natives/#_0xAE99FB955581844A
             SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, true, true, false) 
         end 	
     end 
